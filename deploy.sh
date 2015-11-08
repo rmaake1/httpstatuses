4130e2f7c84584d1fec2a85803594395392794f6
Samuel Ryan <sam@samryan.co.uk>
2015-11-08 00:28:14 +0000

    Fixes project name reference

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 build="build-${CIRCLE_BUILD_NUM}"
 sudo mkdir ~/${build}
-sudo cp -R ~/httpstatuses.com/build/. ~/${build}
+sudo cp -R ~/httpstatuses/build/. ~/${build}
 echo "Deploying ${build} to ${DEPLOY_TARGET}"
 rsync -r -l ~/${build} ${DEPLOY_TARGET}:/srv/www/httpstatuses.com
 ssh ${DEPLOY_TARGET} "ln -sfn /srv/www/httpstatuses.com/${build} /srv/www/httpstatuses.com/live"
