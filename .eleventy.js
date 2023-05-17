89a670a977f8e5699f4f1c644c38b85d3fd0e9c0
Jon Kulton <jonkulton@gmail.com>
2023-05-17 19:17:12 -0400

    update excerpt behavior

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,22 @@
 const _ = require('lodash');
+const excerpt = require('./lib/filters/excerpt');
+const prune = require('voca/prune');
 
 module.exports = function(eleventyConfig) {
   ["contents/**.ico", "contents/**.txt"]
     .forEach(file => eleventyConfig.addPassthroughCopy(file))
 
+  eleventyConfig.addFilter("excerpt", content => prune(content, 400));
+
   // Manipulate collection to group status codes by category
   eleventyConfig.addCollection("codes", async function(collectionApi) {
-      return _.chain(collectionApi.getFilteredByGlob("./contents/codes/*.md"))
-          .orderBy((post) => post.data.code)
-          .groupBy((post) => post.data.set)
-          .value();
+    return _.chain(collectionApi.getFilteredByGlob("./contents/codes/*.md"))
+      .orderBy((post) => post.data.code)
+      .groupBy((post) => post.data.set)
+      .value();
   });
 
   return {
-    dir: {
-      input: "contents"
-    }
+    dir: { input: "contents" }
   }
 };
